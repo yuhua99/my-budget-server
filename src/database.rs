bd73fd806f09deb8330c42b5b616d4d718054149
yuhuahung <yuhuahung@synology.com>
2025-07-04 15:47:46 +0800

    feat: return connection instead of db

--- src/database.rs
+++ src/database.rs
@@ -1,19 +1,32 @@
 use anyhow::Result;
-use libsql::{Builder, Database};
+use libsql::{Builder, Connection};
 use std::{path::Path, sync::Arc};
+use tokio::sync::Mutex;
+
+const CREATE_USERS_TABLE: &str = r#"
+CREATE TABLE IF NOT EXISTS users (
+    id             INTEGER PRIMARY KEY AUTOINCREMENT,
+    name           TEXT    NOT NULL,
+    password_hash  TEXT    NOT NULL
+);
+"#;
 
 /// Main users registry DB (users.db)
-pub async fn init_main_db(data_dir: &str) -> Result<Arc<Database>> {
+pub async fn init_main_db(data_dir: &str) -> Result<Arc<Mutex<Connection>>> {
     tokio::fs::create_dir_all(data_dir).await?;
     let path = Path::new(data_dir).join("users.db");
     let db = Builder::new_local(path).build().await?;
-    Ok(Arc::new(db))
+    let conn = db.connect()?;
+
+    conn.execute(CREATE_USERS_TABLE, ()).await?;
+    Ok(Arc::new(Mutex::new(conn)))
 }
 
 /// Per-user isolated DB (user_{id}.db)
-pub async fn get_user_db(data_dir: &str, user_id: i32) -> Result<Arc<Database>> {
+pub async fn get_user_db(data_dir: &str, user_id: i32) -> Result<Arc<Mutex<Connection>>> {
     let path = Path::new(data_dir).join(format!("user_{}.db", user_id));
     let db = Builder::new_local(path).build().await?;
-    Ok(Arc::new(db))
+    let conn = db.connect()?;
+    Ok(Arc::new(Mutex::new(conn)))
 }
 
